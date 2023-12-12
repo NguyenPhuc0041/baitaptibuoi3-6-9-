afb099ba81eda40b0ec1983e44e1796304a662af
Nguyen Phuc Duong <Nguyen Phuc Duong@DESKTOP-8T9G6GN>
2023-12-12 21:52:36 +1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace baitaptibuoi3_6_10_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Bai tap 6
+            //Console.WriteLine("Nhap vao so nguyen n = ");
+            //int n = int.Parse(Console.ReadLine());
+            //int s = 0;
+            //for (int i = 1; i <= n; i++)
+            //    s = s + (int)Math.Pow(i, i);
+            //Console.WriteLine("Tong S theo cong thuc la {0}", s);
+            //Console.ReadKey();
+
+            //Bai tap 7
+            //Console.WriteLine("Nhap vao so nguyen n = ");
+            //int n = int.Parse(Console.ReadLine());
+            //int s = 0;
+            //for (int i = 0; i <= n; i++) 
+            //{
+            //    if ( i % 2 != 0) 
+            //            s = s + i;
+            //}
+            //Console.WriteLine("Tong cac so le tu 1 den {0} = {1}",n, s);
+            //Console.ReadKey();
+
+            //Bai tap 8
+            //Console.WriteLine("Nhap vao so nguyen n = ");
+            //int n = int.Parse(Console.ReadLine());
+            //int s = 0;
+            //for (int i = 2; i <= n; i++)
+            //{
+            //    if (i % i == 0 && i % 1 == 0)
+
+            //}
+
+
+
+
+            //    //bai tap 9
+            //    Console.WriteLine("Nhap vao so hang n = ");
+            //    int n = int.Parse(Console.ReadLine());
+            //    string str = "";
+            //    for (int i=1; i <=n; i++)
+            //    {
+            //        str = string.Concat(str, "*");
+            //        Console.WriteLine(str);
+
+            //    }
+            //    Console.ReadKey();
+            //}
+        }
+}
